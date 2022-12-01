19ab8ec652b48b17341247a61b63f48c2656f38d
jeongyeon.seo <108205639+yeon1128@users.noreply.github.com>
2022-12-01 23:42:03 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let answer = 1;
+  for (let i = 1; i <= n; i++) {
+    answer *= i;
+    if (answer === n) {
+      return i;
+    }
+    if (answer > n) {
+      return i - 1;
+    }
+  }
+}
